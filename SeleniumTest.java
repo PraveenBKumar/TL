a02dca10cb9ea9e64a25a83482c09ddf5859a4e4
Praveen Kumar <praveenbkumar1993@gmail.com>
2020-07-15 23:02:08 +0530

    tl

---
+++ SeleniumTest.java
@@ -0,0 +1,45 @@
+package com.selenium.test;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SeleniumTest {
+
+	WebDriver driver;
+
+	@Test
+	public void webdriverie() throws Exception {
+		WebDriverManager.iedriver().setup();
+		driver = new InternetExplorerDriver();
+		driver.get("https://www.amazon.in/");
+		driver.quit();
+		try {
+			Runtime.getRuntime().exec("taskkill /F /IM IEDriverServer.exe");
+			} catch (IOException e) {
+			e.printStackTrace();
+			}
+	}
+	
+	/*@Test
+	public void webdriverff() throws Exception {
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
+		driver.get("https://www.amazon.in/");
+		driver.quit();
+	}
+	
+	@Test
+	public void webdriverch() throws Exception {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		driver.quit();
+	}*/
+}
